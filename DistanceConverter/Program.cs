cfa3a58b5cc51124ff76340f85008ef3cb760a67
shusuke <32261@ojs.ac.jp>
2021-05-21 13:37:37 +0900

    プロジェクト ファイルを追加します。

---
+++ DistanceConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int feet = 0; feet <= 10; feet++) {
+
+            }
+
+        }
+    }
+}
