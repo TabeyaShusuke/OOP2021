4770fe301300b3431b6cf80c7341cffcc763b816
shusuke <32261@ojs.ac.jp>
2021-05-21 15:18:54 +0900

    p49 list2.3

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -8,10 +8,28 @@ namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
 
-            for (int feet = 0; feet <= 10; feet++) {
-
+            if (args.Length >= 1&& args[0] == "-tom") {
+                for (int feet = 1; feet <= 10; feet++) {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+                }
+            } else {
+                  for (int meter = 1; meter <= 10; meter++) {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} m = {1:0.0000} ft",meter,feet);
+                  }  
             }
+        }
 
+
+        //feetからmを求める
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        //mからfeetを求める
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
         }
     }
 }
