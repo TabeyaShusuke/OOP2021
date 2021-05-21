92730552dc01398709dec4ea48d66d4b7d90e5a7
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-21 15:53:05 +0900

    p49 list2.4

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -9,15 +9,26 @@ namespace DistanceConverter {
         static void Main(string[] args) {
 
             if (args.Length >= 1&& args[0] == "-tom") {
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
-                }
+                PrintFeetToMeterList(10,110);
             } else {
-                  for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000} ft",meter,feet);
-                  }  
+                PrintMeterToFeetList(10,110);
+            }
+        }
+
+        //feet to m
+        private static void PrintFeetToMeterList(int start,int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+
+
+        //m to feet
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }
 
