c5f892b01ce8b5336a5a194348974b4d3bda7b0a
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-25 10:30:21 +0900

    p53 list 2.7 静的メソッドに変更

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -18,10 +18,8 @@ namespace DistanceConverter {
         //feet to m
         private static void PrintFeetToMeterList(int start,int stop) {
 
-            FeetConverter f = new FeetConverter();
-
             for (int feet = start; feet <= stop; feet++) {
-                double meter = f.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
@@ -29,10 +27,8 @@ namespace DistanceConverter {
         //m to feet
         private static void PrintMeterToFeetList(int start, int stop) {
 
-            FeetConverter f = new FeetConverter();
-
             for (int meter = start; meter <= stop; meter++) {
-                double feet = f.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }
