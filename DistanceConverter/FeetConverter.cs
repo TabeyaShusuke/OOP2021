a3f87d136b64c4d6f9c4b42fba5cd899be0bd1b9
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-25 11:06:03 +0900

    p56 list 2.10 定数定義（0.3048）　 ソース完成

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -7,14 +7,17 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     //feet m の単位変換クラス（静的クラス）
     static class FeetConverter {
+
+        private const double ratio = 0.3048;
+
         //m to feet
         public　static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //feet to m
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
 
     }
