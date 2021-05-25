3b502aae263224955370e7eaf7b5074ded06964d
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-25 10:35:43 +0900

    p53 list2.7

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    class FeetConverter {
+    //feet m の単位変換クラス（静的クラス）
+    static class FeetConverter {
         //m to feet
         public　static double FromMeter(double meter) {
             return meter / 0.3048;
