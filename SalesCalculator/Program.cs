c7166fa4e16c4927542d224ece34d4f2a20027ff
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-27 15:50:47 +0900

    p70 list2.22

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+            
+            var sales = new SalesCounter("Sales.csv");
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string,int> obj in amountPerStore) {
                 Console.WriteLine("{0},{1}",obj.Key,obj.Value);
@@ -16,21 +17,6 @@ namespace SalesCalculator {
 
         }
 
-        //売り上げデータを読み込みSaleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) { 
-            List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines){
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-	        
-            }
-            return sales;
-        }        
+        
     }
 }
--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace SalesCalculator {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         public Dictionary<string,int> GetPerStoreSales() {
@@ -25,6 +26,23 @@ namespace SalesCalculator {
                     dict[sale.ShopName] = sale.Amount;
             } 
             return dict;
+        }
+
+        //売り上げデータを読み込みSaleオブジェクトのリストを返す
+        private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+
+            }
+            return sales;
         }
     }
 }
