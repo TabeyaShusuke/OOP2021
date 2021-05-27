8bace3a9f816a6a12e0cad143391dc43a2b8343b
TabeyaShusuke <32261@ojs.ac.jp>
2021-05-27 15:21:01 +0900

    p68 list 2.19 初版完成

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,7 +8,11 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            //List<Sale> sales = ReadSales("sales.csv");
+            SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string,int> obj in amountPerStore) {
+                Console.WriteLine("{0},{1}",obj.Key,obj.Value);
+            }
 
         }
 
