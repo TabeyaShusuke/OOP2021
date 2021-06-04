61501ec43775cd22bd5f02ed54a4e8c08d6faa5f
TabeyaShusuke <32261@ojs.ac.jp>
2021-06-04 14:37:33 +0900

    p99,100 chapter3  practice

---
+++ Practice/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+            // 3.2.1
+            Exercise2_1(names);
+            Console.WriteLine("-----");
+
+            // 3.2.2
+            Exercise2_2(names);
+            Console.WriteLine("-----");
+
+            // 3.2.3
+            Exercise2_3(names);
+            Console.WriteLine("-----");
+
+            // 3.2.4
+            Exercise2_4(names);
+            Console.WriteLine("-----");
+
+
+            //3.3.1
+            Exercise3_1(names);
+            Console.WriteLine("-----");
+        }
+
+        private static void Exercise3_1(List<string> names) {
+            
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            var bnames = names.Where(s => s.ToString().StartsWith("B")).Select(s => s.Length);
+            foreach (var n in bnames) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            var extraction = names.Where(s => s.ToString().Contains('o'));
+            foreach (var oname in extraction) {
+                Console.WriteLine(oname);
+            }
+            
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.Write("都市名:");
+            var Line = Console.ReadLine();
+            var search = names.FindIndex(s => s == Line);
+                Console.WriteLine(search);
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var list = numbers.Select(s => s * 2).ToList();
+            foreach (var dos in list) {
+                Console.WriteLine(dos);
+            }
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            foreach (var num in numbers.Where(s => 50 <= s)){
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(n => Console.WriteLine( n/2.0));
+            
+
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exists = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            if (exists)
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在しない");
+
+        }
+    }
+}
---
+++ Section01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            //var names = new List<string> {
+            //     "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            //};
+
+            //IEnumerable<string> query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
+            //foreach (string s in query) {
+            //    Console.WriteLine(s);
+            //}
+
+
+            string[] names = {
+                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"  };
+            var query = names.Where(s => s.Length <= 5).ToList();
+
+
+
+            foreach (var item in query)
+                Console.WriteLine(item);
+            Console.WriteLine("------");
+
+            names[0] = "Osaka";
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+        }
+    }
+}
