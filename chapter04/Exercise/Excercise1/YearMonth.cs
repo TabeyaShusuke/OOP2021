7f2d1eba75f2ad44ac80b8ea97ef5c4036606bdd
TabeyaShusuke <32261@ojs.ac.jp>
2021-06-10 15:43:10 +0900

    p134 chapter4 practice 4.2.2まで

---
+++ chapter04/Exercise/Excercise1/YearMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1 {
+    public class YearMonth {
+        //1
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+        
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        //3
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(this.Year+1,1) ;
+            } else {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+        }
+
+        public override string ToString() {
+            return Year+"年"+Month+"月";
+        }
+    }
+}
---
+++ chapter04/Exercise/Excercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Excercise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2 {
+    class Program {
+        static void Main(string[] args) {
+           var ym1 = new YearMonth(1999,3);
+           var ym2 = new YearMonth(1999,4);
+           var ym3 = new YearMonth(1999,5);
+           var ym4 = new YearMonth(1999,6);
+           var ym5 = new YearMonth(1999,12);
+            var yms = new YearMonth[] {ym1,ym2,ym3,ym4,ym5,};
+
+            for (int i = 0; i < yms.Length; i++) {
+                Console.WriteLine(yms[i]);
+            }
+
+
+
+        }
+    }
+}
