b3fc9a3741e9e1b4a3e900f13a31b6f591f78973
TabeyaShusuke <32261@ojs.ac.jp>
2021-06-11 15:38:21 +0900

    p158 practice5.1 done

---
+++ Chapter05/Excercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("文字列1:");
+            var st1 = Console.ReadLine();
+            Console.Write("文字列2:");
+            var st2 = Console.ReadLine();
+
+            if (string.Compare(st1, st2, true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+            
+        }
+    }
+}
