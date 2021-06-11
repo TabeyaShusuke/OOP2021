a87fc04c5a5b3d3acbd9bc5d4db09b37758929d5
TabeyaShusuke <32261@ojs.ac.jp>
2021-06-11 15:52:04 +0900

    p158 Q5.2 途中

--- Chapter05/Excercise1/Program.cs
+++ Chapter05/Excercise1/Program.cs
@@ -7,16 +7,26 @@ using System.Threading.Tasks;
 namespace Excercise1 {
     class Program {
         static void Main(string[] args) {
+#if false
+#region 問題5.1
             Console.Write("文字列1:");
             var st1 = Console.ReadLine();
             Console.Write("文字列2:");
             var st2 = Console.ReadLine();
-
-            if (string.Compare(st1, st2, true) == 0)
+             
+            if (string.Compare(st1, st2, ignoreCase:true) == 0)
                 Console.WriteLine("等しい");
             else
                 Console.WriteLine("等しくない");
-            
+#endregion
+#endif
+
+#region 問題5.2
+            Console.Write("数値:");
+            var str = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0:#,0}",str);
+#endregion
+
         }
     }
 }
