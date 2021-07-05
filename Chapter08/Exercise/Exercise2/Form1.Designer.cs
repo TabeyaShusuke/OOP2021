bf2900695137724457ced482aaf0d49364665244
TabeyaShusuke <32261@ojs.ac.jp>
2021-07-05 15:56:22 +0900

    stopwatch

---
+++ Chapter08/Exercise/Exercise2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Exercise2 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.start.Location = new System.Drawing.Point(45, 222);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(118, 78);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.stop.Location = new System.Drawing.Point(45, 395);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(118, 64);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lb1.Location = new System.Drawing.Point(41, 91);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(281, 32);
+            this.lb1.TabIndex = 3;
+            this.lb1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(910, 520);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label lb1;
+    }
+}
+
---
+++ Chapter08/Exercise/Exercise2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2 {
+    public partial class Form1 : Form {
+
+        //stopwatch 
+        Stopwatch sw = new Stopwatch();
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            lb1.Text = sw.Elapsed.ToString(@"hh\:mm\:ss\.ff");
+        }
+
+        private void start_Click(object sender, EventArgs e) {
+            Timer tm = new Timer();
+            tm.Tick += Tm_Tick;
+            tm.Interval = 100;
+            sw.Start();
+
+        }
+
+        private void Tm_Tick(object sender, EventArgs e) {
+            
+            lb1.Text = sw.ToString();
+        }
+
+        private void stop_Click(object sender, EventArgs e) {
+            sw.Stop();
+        }
+    }
+}
