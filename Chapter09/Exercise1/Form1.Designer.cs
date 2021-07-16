974fa57e8a3e836660a58bc68bc053fefc195986
shusuke <32261@ojs.ac.jp>
2021-07-16 15:59:00 +0900

    思考ができないプログラム

---
+++ Chapter09/Exercise1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Exercise1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btReadAllLines = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(12, 91);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(776, 347);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(12, 25);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(106, 50);
+            this.btOpen.TabIndex = 1;
+            this.btOpen.Text = "開く...";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(143, 56);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(133, 19);
+            this.tbKey.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "キーワード";
+            // 
+            // btReadAllLines
+            // 
+            this.btReadAllLines.Location = new System.Drawing.Point(324, 25);
+            this.btReadAllLines.Name = "btReadAllLines";
+            this.btReadAllLines.Size = new System.Drawing.Size(138, 57);
+            this.btReadAllLines.TabIndex = 4;
+            this.btReadAllLines.Text = "ReadAllLines";
+            this.btReadAllLines.UseVisualStyleBackColor = true;
+            this.btReadAllLines.Click += new System.EventHandler(this.btReadAllLines_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btReadAllLines);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.tbOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btReadAllLines;
+    }
+}
+
---
+++ Chapter09/Exercise1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btOpen_Click(object sender, EventArgs e) {
+            if (ofdOpenFile.ShowDialog() == DialogResult.OK ){
+                int count = 0;
+                using (var reader = new StreamReader(ofdOpenFile.FileName,Encoding.GetEncoding("shift_jis"))) {
+                    while (!reader.EndOfStream) {
+                        var line = reader.ReadLine();//1row
+                        if (line.Contains(tbKey.Text)) {
+                            count++;
+                        }
+                        //textbox
+                        tbOutput.Text = tbKey.Text + count.ToString();
+                        //class
+                    }
+                }
+            }
+        }
+
+        private void btReadAllLines_Click(object sender, EventArgs e) {
+            if (ofdOpenFile.ShowDialog() == DialogResult.OK) {
+                var lines = File.ReadAllLines(ofdOpenFile.FileName, Encoding.GetEncoding("shift_jis"));
+            }
+        }
+    }
+}
