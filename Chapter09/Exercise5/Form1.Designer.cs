8647e53a7a7d158ffc1b29376da27c4124dfbf39
shusuke <32261@ojs.ac.jp>
2021-08-23 15:51:13 +0900

    累乗

--- Chapter09/Exercise5/Form1.Designer.cs
+++ Chapter09/Exercise5/Form1.Designer.cs
@@ -58,7 +58,7 @@ namespace Exercise5 {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(597, 450);
             this.Controls.Add(this.tbDisplay);
             this.Controls.Add(this.tbDir);
             this.Controls.Add(this.btOpen);
---
+++ Sample0823/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e) {
+            int n = int.Parse(Value.Text);
+            int m = int.Parse(Jyou.Text);
+            Result.Text = Math.Pow(n,m).ToString();
+        }
+    }
+}
