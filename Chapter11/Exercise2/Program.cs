3df0a5ba0c97c8d545a24aeef8425a1ab152f2b6
shusuke <32261@ojs.ac.jp>
2021-08-30 16:01:01 +0900

    章末問題　11.2

---
+++ Chapter11/Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("11_2.xml");
+            var words = xdoc.Root.Elements()
+                                 .Select(x => new XElement("word",
+                                 new XAttribute("kanji",x.Element("kanji").Value),
+                                 new XAttribute("yomi", x.Element("yomi").Value))
+                                 );
+            var root = new XElement("difficultkanji", words);
+            root.Save("Sample.xml");
+        }
+    }
+}
