ab8b2a6f243817dea07112564f53153a8beaa933
shusuke <32261@ojs.ac.jp>
2021-10-17 10:37:09 +0900

    13章　章末問題

--- EntityFramework/SampleEntityFramework/Program.cs
+++ EntityFramework/SampleEntityFramework/Program.cs
@@ -17,7 +17,6 @@ namespace SampleEntityFramework {
                 
             }
 
-
             using (var db = new BooksDbContext()) {
                 //書籍2冊以上の著者取得
                 var authors = db.Authors.Where(a => a.Books.Count() >= 2);
@@ -47,23 +46,108 @@ namespace SampleEntityFramework {
                 Console.WriteLine($"{auth.Name}{auth.Gender}{auth.Birthday}");
             }
 
+            //1
             Addname();
-            
+
+            //2
+            foreach (var book in GetData()) {
+                Console.WriteLine($"{book.Id}{book.Title}{book.PublishedYear}" +
+                    $"{book.Author.Name}{book.Author.Birthday}{book.Author.Gender}");
+            }
+
+            MaxlengthTitle();
+
+            //4
+            OldTitle();
+
+            //5
+            DescBirth();
         }
 
+        private static void DescBirth() {
+            using (var db = new BooksDbContext()) {
+                var books = db.Books.OrderByDescending(s => s.Author.Birthday);
+                foreach (var book in books) {
+                    Console.WriteLine($"{book.Author.Name}{book.Title}{book.PublishedYear}");
+                }
+            }
+        }
+
+        private static void OldTitle() {
+            using (var db = new BooksDbContext()) {
+                var books = db.Books.OrderBy(s => s.PublishedYear).Take(3);
+                foreach (var book in books) {
+                    Console.WriteLine($"{book.Title}{book.Author.Name}");
+                }
+            }
+        }
+
+        //3
+        private static void MaxlengthTitle() {
+            using (var db = new BooksDbContext()) {
+                var author = db.Books.Max(s => s.Title);
+                foreach (var book in author) {
+                    Console.WriteLine($"{book}");
+                }
+            } 
+        }
+
+        private static IEnumerable<Book> GetData() {
+            using (var db = new BooksDbContext()) {
+                return db.Books.Include(nameof(Author)).ToList();
+            }
+        }
+
+        //章末問題1
         private static void Addname() {
-            //using (var db = new BooksDbContext()) {
-            //    var book1 = new Book {
-            //        Title = "坊ちゃん",
-            //        PublishedYear = 2003,
-            //        Author = new Author {
-            //            Birthday = new DateTime(1867, 2, 9),
-            //            Gender = "M",
-            //            Name = "夏目漱石",
-            //        }
-            //    };
-            //    db.Books.Add(book1);
-            //}
+            using (var db = new BooksDbContext()) {
+                var author1 = new Author {
+                    Birthday = new DateTime(1888, 12, 26),
+                    Gender = "M",
+                    Name = "菊池寛",
+                };
+                db.Authors.Add(author1);
+
+                var author2 = new Author {
+                    Birthday = new DateTime(1899, 6, 14),
+                    Gender = "M",
+                    Name = "川端康成",
+                };
+                db.Authors.Add(author2);
+
+                var author3 = db.Authors.Single(a => a.Name == "夏目漱石");
+                var book1 = new Book {
+                    Title = "こころ",
+                    PublishedYear = 1991,
+                    Author = author3,
+                };
+                db.Books.Add(book1);
+
+                var author4 = db.Authors.Single(a => a.Name == "川端康成");
+                var book2 = new Book {
+                    Title = "伊豆の踊子",
+                    PublishedYear = 2003,
+                    Author = author4,
+                };
+                db.Books.Add(book2);
+
+                var author5 = db.Authors.Single(a => a.Name == "菊池寛");
+                var book3 = new Book {
+                    Title = "真珠婦人",
+                    PublishedYear = 2002,
+                    Author = author5,
+                };
+                db.Books.Add(book3);
+
+                var author6 = db.Authors.Single(a => a.Name == "宮沢賢治");
+                var book4 = new Book {
+                    Title = "注文の多い料理店",
+                    PublishedYear = 2000,
+                    Author = author6,
+                };
+                db.Books.Add(book4);
+                db.SaveChanges();
+            }
         }
 
         //13-14
@@ -114,7 +198,6 @@ namespace SampleEntityFramework {
 
             }
         }
-
 
         //13-7
         static IEnumerable<Book> GetBooks() {
